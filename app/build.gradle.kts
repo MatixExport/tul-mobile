53a6bb072117db86c69cbfbb844e3c9de3b3dac8
Jakub Kubiś <jakubkubis45@gmail.com>
2024-12-16 17:08:58 +0100

    update app package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "indie.outsource.ai"
         minSdk = 27
         targetSdk = 35
         versionCode = 1
